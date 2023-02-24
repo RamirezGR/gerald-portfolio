2b7a32d732cfc39234cd2ff66caa95db2c6a7435
kulotzkie980@gmail.com <kulotzkie980@gmail.com>
2023-02-24 11:11:50 +0800

    finalize socmed links

--- src/components/Socmed/Socmed.jsx
+++ src/components/Socmed/Socmed.jsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import {Link} from 'react-router-dom'
 import {FaFacebookF, FaLinkedinIn} from 'react-icons/fa'
 import {FiGithub} from 'react-icons/fi'
 import './Socmed.css'
@@ -10,25 +9,24 @@ const Socmed = () => {
     <>
         <div>
             <ul className="wrapper">
-                <Link
-                 to ="www.facebook.com/gerald.ase" target={'_blank'}>
+                <a href="https://www.facebook.com/gerald.ase" target="_blank">
                     <li className="icon facebook">
                         <span className='tooltip'>Facebook</span>
                         <span><FaFacebookF/></span>
                     </li>
-                </Link>
-                <Link to="www.linkedin.com/in/gerald-ramirez" target={'_blank'}>
+                </a>
+                <a href="https://www.linkedin.com/in/gerald-ramirez" target='_blank'>
                     <li className="icon linkedin">
                         <span className='tooltip'>LinkedIn</span>
                         <span><FaLinkedinIn/></span>
                     </li>
-                </Link>
-                <Link to="github.com/RamirezGR" target={'_blank'}>
+                </a>
+                <a href="https://github.com/RamirezGR" target={'_blank'}>
                     <li className="icon github">
                         <span className='tooltip'>Github</span>
                         <span><FiGithub/></span>
                     </li>
-                </Link>
+                </a>
             </ul>
         </div>
         <div className="email-side">
